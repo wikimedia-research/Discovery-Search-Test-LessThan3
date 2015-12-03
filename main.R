2d845ffacf34f3959526ffe1ba7b64128ae8f841
ironholds <ironholds@gmail.com>
2015-12-03 08:16:51 -0500

    add

--- main.R
+++ main.R
@@ -40,9 +40,9 @@ main <- function(){
   
   # Filter and do exploratory analysis
   data <- data[data$date %in% seq(as.Date("2015-11-21"), as.Date("2015-11-27"), "day"),]
-  data$group[data$group == "multilang-it-n-a"] <- "Control"
-  data$group[data$group == "multilang-it-n-b"] <- "<1"
-  data$group[data$group == "multilang-it-n-c"] <- "<3"
+  data$group[data$group == "multilang-lt-n-a"] <- "Control"
+  data$group[data$group == "multilang-lt-n-b"] <- "<1"
+  data$group[data$group == "multilang-lt-n-c"] <- "<3"
   
   per_group <- data[, j = list(events = sum(events)), by = c("date", "group")]
   ggsave(file = "events_by_group_summary.png",
@@ -57,5 +57,124 @@ main <- function(){
                 x = "Source", y = "Events"),
          file = "events_by_source_summary.png")
   
+  # Function for performing necessary analysis
+  analyse_ab_data <- function(dataset, groups, field){
+    
+    output <- list()
+    
+    # High-level data
+    high_level <- dataset[dataset$group %in% groups, j = sum(events), by = c(field, "group")]
+    high_level <- high_level[order(high_level[,1, with = FALSE], high_level$group, decreasing = T),]
+    
+    # Generate count table
+    count_table <- matrix(data = high_level$V1, nrow = 2, byrow = FALSE,
+                          dimnames=list("Group" = unique(high_level$group),
+                                        "Outcome" = c("Results", "Zero results")))
+    
+    # Write out prop_diff_tail and relative_risk
+    output$high_level_prop_diff <- ci_prop_diff_tail(count_table)
+    output$high_level_risk <- ci_relative_risk(count_table)
+    
+    # Split by web and API, if applicable
+    api_data <- dataset[dataset$source == "api",]
+    if(nrow(api_data)){
+      api_data <- api_data[api_data$group %in% groups, j = sum(events), by = c(field, "group")]
+      api_data <- api_data[order(api_data[,1, with = FALSE], api_data$group, decreasing = T),]
+      count_table <- matrix(data = api_data$V1, nrow = 2, byrow = FALSE,
+                            dimnames=list("Group" = unique(api_data$group),
+                                          "Outcome" = c("Results", "Zero results")))
+      output$api_prop_diff <- ci_prop_diff_tail(count_table)
+      output$api_risk <- ci_prop_diff_tail(count_table)
+    }
+    
+    web_data <- dataset[dataset$source == "web",]
+    if(nrow(web_data)){
+      web_data <- web_data[web_data$group %in% groups, j = sum(events), by = c(field, "group")]
+      web_data <- web_data[order(web_data[,1, with = FALSE], web_data$group, decreasing = T),]
+      count_table <- matrix(data = web_data$V1, nrow = 2, byrow = FALSE,
+                            dimnames=list("Group" = unique(web_data$group),
+                                          "Outcome" = c("Results", "Zero results")))
+      output$web_prop_diff <- ci_prop_diff_tail(count_table)
+      output$web_risk <- ci_prop_diff_tail(count_table)
+    }
+    
+    return(output)
+  }
   
+  # basic analysis of the <1 data
+  non_zero_basic_results <- analyse_ab_data(data, groups = c("Control", "<1"), field = "non_zero")
+  
+#   $high_level_prop_diff
+#   [1] -0.02949650 -0.02884083
+#   
+#   $high_level_risk
+#   [1] 0.964146 1.061427
+#   
+#   $api_prop_diff
+#   [1] -0.0342201 -0.0334806
+#   
+#   $api_risk
+#   [1] -0.03422089 -0.03348276
+#   
+#   $web_prop_diff
+#   [1] -0.01259161 -0.01114163
+#   
+#   $web_risk
+#   [1] -0.01259145 -0.01114222
+#   
+
+  # basic analysis of the <1 data that hits the langdetect parameter
+  langdetect_data <- data[data$langdetect == TRUE,]
+  non_zero_langdetect_results <- analyse_ab_data(langdetect_data, groups = c("Control", "<1"), field = "non_zero")
+  
+#   $high_level_prop_diff
+#   [1] 0.224984 0.227874
+#   
+#   $high_level_risk
+#   [1] 3.541259 3.614576
+#   
+#   $web_prop_diff
+#   [1] 0.2249853 0.2278746
+#   
+#   $web_risk
+#   [1] 0.2249846 0.2278656
+  
+  # Okay, basic analysis of <3 data
+  lessthan_basic_results <- analyse_ab_data(data, groups = c("Control", "<3"), field = "more_than_3")
+  
+#   $high_level_prop_diff
+#   [1] 0.01170693 0.01244354
+#   
+#   $high_level_risk
+#   [1] 1.016063 1.017077
+#   
+#   $api_prop_diff
+#   [1] 0.0239356 0.0247683
+#   
+#   $api_risk
+#   [1] 0.02393818 0.02476878
+#   
+#   $web_prop_diff
+#   [1] -0.03305317 -0.03145465
+#   
+#   $web_risk
+#   [1] -0.03304728 -0.03145821
+  
+  # Langdetect only
+  lessthan_langdetect_results <- analyse_ab_data(langdetect_data, groups = c("Control", "<3"), field = "more_than_3")
+  
+#   $high_level_prop_diff
+#   [1] -0.10158554 -0.09299385
+#   
+#   $high_level_risk
+#   [1] 0.03258774 0.03211152
+#   
+#   $web_prop_diff
+#   [1] -0.10158554 -0.09303557
+#   
+#   $web_risk
+#   [1] -0.10158554 -0.09302172
+  
+  # Write out to be safe
+  save(list = ls(pattern = "_results"), file = "results.RData")
 }
